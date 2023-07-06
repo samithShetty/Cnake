4c349ea070ce7d738c22137809aba2351949184b
Samith Shetty <71335825+samithShetty@users.noreply.github.com>
2023-07-06 18:04:04 -0400

    Implemented Basic Window Loader

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -1,10 +1,53 @@
 #include <windows.h>
 
-LRESULT CALLBACK window_callback( HWND hWnd, UINT Msg, WPARAM wParam,LPARAM lParam) {
-    return DefWindowProc(hWnd,Msg, wParam, lParam);
+bool isRunning = true;
+
+struct Render_State {
+    int width, height;
+    void* memory;
+
+    BITMAPINFO bitmap_info; 
+};
+
+Render_State render_state;
+
+LRESULT CALLBACK window_callback(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
+    LRESULT result = 0;
+
+    switch (Msg) {
+        // Window Closed
+        case WM_CLOSE:
+        case WM_DESTROY: {
+            isRunning = false;
+        } break;
+
+        // Window Resized
+        case WM_SIZE: {
+            RECT rect;
+            GetClientRect(hWnd, &rect);
+            render_state.width = rect.right - rect.left;
+            render_state.height = rect.bottom - rect.top;
+            int size = render_state.width * render_state.height * sizeof(unsigned int);
+
+            if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+            render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+            render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+            render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+            render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+            render_state.bitmap_info.bmiHeader.biPlanes = 1;
+            render_state.bitmap_info.bmiHeader.biBitCount = 32;
+            render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+        }
+
+        default:
+            result = DefWindowProc(hWnd, Msg, wParam, lParam);
+    }
+
+    return result;
 }
 
-int WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nShowCmd) {
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+    
     // Create Window Class
     WNDCLASS window_class = {};
     window_class.style = CS_HREDRAW | CS_VREDRAW;
@@ -12,5 +55,29 @@ int WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nSho
     window_class.lpfnWndProc = window_callback;
 
     // Register Class
-    //RegisterClass(&window_class);
+    RegisterClass(&window_class);
+
+    //Create Window
+    HWND window = CreateWindow(window_class.lpszClassName, "My First Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+    HDC hdc = GetDC(window);
+
+    while (isRunning) {
+        // Input
+        MSG message;
+        while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&message);
+            DispatchMessage(&message);
+        }
+
+        // Update
+        unsigned int* pixel = (unsigned int*)render_state.memory;
+
+        for (int i = 0; i < render_state.width * render_state.height; ++i) {
+            *pixel++ = 0x200F20;
+        }
+
+        // Render
+        StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+    
+    }
 }
